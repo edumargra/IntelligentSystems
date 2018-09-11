48b02bf9a7f4ee72572047de8a273405dd63a35e
S3834549 <S3834549@XAP022.workspace.rug.nl>
2018-09-11 17:33:21 +0200

    Added plot of multiple cities(not working)

--- test.m
+++ test.m
@@ -15,17 +15,32 @@ xlabel('Temperature Values','fontsize',16);
 ylabel('<l> values','fontsize',16);
 %}
 
-z = 1;
-j = 0.5:-0.01:0;
-results = zeros(length(j),2);
-for i = j
-    results(z,:) = tsp(50,100,i,1);
-    z = z + 1;
-end
+j = 0.5:-0.01:0; %temp
+k = 50:10:100; %cities
 
-disp(results)
+results = zeros(length(j),2);
+finalResults = zeros(length(j),length(k)*2);
+column = 1;
+for l = k
+    z = 1;
+    for i = j
+        results(z,:) = tsp(l,100,i,1);
+        z = z + 1;
+    end
+    finalResults(:,column:column+1) = results;
+    column = column + 2;
+end
+disp(finalResults)
 figure(3); plot(0,0); hold on; 
-errorbar(j,results(:,1),results(:,2),'ko-');
+%fem grafic de les diferents ciutsts
+
+colours = ['ko-' 'ro-' 'bo-' 'mo-' 'go-'];
+for column = 1:2:length(k)
+    errorbar(j,finalResults(:,column), finalResults(:,column+1),colours(mod(column,length(colours))+1));
+    hold on;
+end
+    
+%errorbar(j,results(:,1),results(:,2),'ko-');
 title('<l> vs T comparsion','fontsize',16);
 xlabel('Temperature Values','fontsize',16);
 ylabel('<l> values','fontsize',16);
