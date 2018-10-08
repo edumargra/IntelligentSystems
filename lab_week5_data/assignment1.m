ca6c93033e0fc8b6e7c1b6a45ead3fe896df03c0
s3834549 <s3834549@XAP025.workspace.rug.nl>
2018-10-08 14:51:20 +0200

    Added code and necessary data

---
+++ lab_week5_data/assignment1.m
@@ -0,0 +1,28 @@
+function assignment1(k,eta,tmax)
+
+X = load('w6_1x.mat');
+Y = load('w6_1y.mat');
+Z = load('w6_1z.mat');
+
+[p,n] = size(X.w6_1x); %data from x,y and z have the same size
+prototype_values = X.w6_1x(randperm(p,k),:)
+figure(1); hold on;
+scatter(X.w6_1x(:,1),X.w6_1x(:,2),'k.');
+scatter(prototype_values(:,1),prototype_values(:,2),'r*');
+for t = 1:tmax
+    values = X.w6_1x(randperm(p),:);
+    for i = 1:p
+        point = values(i,:);
+        distance = zeros(k);
+        for j = 1:k
+            prototype = prototype_values(j,:);
+            distance(j) = (prototype(1)-point(1))^2 + (prototype(2)-point(2))^2;
+        end
+        [~,idx] = min(distance);
+        prototype_values(idx,:) = prototype_values(idx,:) + eta * (point - prototype_values(idx,:));
+    end
+    scatter(prototype_values(idx,1),prototype_values(idx,2),'g.');
+end
+scatter(prototype_values(:,1),prototype_values(:,2),'m*');
+hold off;
+end
