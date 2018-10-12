03a1f32af40fe329d633452a7f83ef0dadfdaf24
S3834549 <S3834549@XAP023.workspace.rug.nl>
2018-10-12 11:09:39 +0200

    Added the image for 2 prototypes, added legend

--- lab_week5_data/assignment1.m
+++ lab_week5_data/assignment1.m
@@ -7,8 +7,8 @@ Z = load('w6_1z.mat');
 [p,n] = size(X.w6_1x); %data from x,y and z have the same size
 prototype_values = X.w6_1x(randperm(p,k),:)
 figure(1); hold on;
-scatter(X.w6_1x(:,1),X.w6_1x(:,2),'k.');
-scatter(prototype_values(:,1),prototype_values(:,2),'r*');
+p1 = scatter(X.w6_1x(:,1),X.w6_1x(:,2),'k.');
+p2 = scatter(prototype_values(:,1),prototype_values(:,2),'r*');
 for t = 1:tmax
     values = X.w6_1x(randperm(p),:);
     for i = 1:p
@@ -21,9 +21,12 @@ for t = 1:tmax
         [~,idx] = min(distance);
         prototype_values(idx,:) = prototype_values(idx,:) + eta * (point - prototype_values(idx,:));
     end
-    scatter(prototype_values(idx,1),prototype_values(idx,2),'g.');
+    p3 = scatter(prototype_values(idx,1),prototype_values(idx,2),'g.');
     drawnow;
 end
-scatter(prototype_values(:,1),prototype_values(:,2),'m*');
+p4 = scatter(prototype_values(:,1),prototype_values(:,2),'m*');
+title('Winner-takes-all implementation on w6_1x set for 2 prototypes');
+h = [p1(1),p2(1),p3(1),p4(1)];
+legend(h,'Data points', 'Initial prototypes', 'Prototypes in each iteration', 'Final prototypes', 'Location', 'northeastoutside');
 hold off;
 end
