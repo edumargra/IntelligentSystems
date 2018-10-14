525fc852ff5ab8f73c38b57cfd56e03d24ca549e
S3836452 <S3836452@XAP030.workspace.rug.nl>
2018-10-15 01:48:14 +0200

    pre-merge

--- lab_week5_data/assignment1.m
+++ lab_week5_data/assignment1.m
@@ -6,6 +6,7 @@ Z = load('w6_1z.mat');
 
 [p,n] = size(X.w6_1x); %data from x,y and z have the same size
 prototype_values = X.w6_1x(randperm(p,k),:)
+h_vq = zeros(tmax);
 figure(1); hold on;
 p1 = scatter(X.w6_1x(:,1),X.w6_1x(:,2),'k.');
 p2 = scatter(prototype_values(:,1),prototype_values(:,2),'r*');
@@ -18,11 +19,13 @@ for t = 1:tmax
             prototype = prototype_values(j,:);
             distance(j) = (prototype(1)-point(1))^2 + (prototype(2)-point(2))^2;
         end
-        [~,idx] = min(distance);
+        [error,idx] = min(distance);
+        h_vq(t) = h_vq(t) + error
         prototype_values(idx,:) = prototype_values(idx,:) + eta * (point - prototype_values(idx,:));
     end
     p3 = scatter(prototype_values(idx,1),prototype_values(idx,2),'g.');
     drawnow;
+    h_vq(t) = 
 end
 p4 = scatter(prototype_values(:,1),prototype_values(:,2),'m*');
 title('Winner-takes-all implementation on w6_1x set for 4 prototypes', 'Interpreter', 'None');
