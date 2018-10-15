d1d5088269c218b1a3ec0cdb8e861c6586e22aff
S3834549 <S3834549@XAP030.workspace.rug.nl>
2018-10-15 02:33:39 +0200

    Added all the images

--- lab_week5_data/assignment1.m
+++ lab_week5_data/assignment1.m
@@ -5,7 +5,7 @@ Y = load('w6_1y.mat');
 Z = load('w6_1z.mat');
 
 [p,n] = size(X.w6_1x); %data from x,y and z have the same size
-prototype_values = X.w6_1x(randperm(p,k),:)
+prototype_values = X.w6_1x(randperm(p,k),:);
 h_vq = zeros(1,tmax);
 figure(1); hold on;
 p1 = scatter(X.w6_1x(:,1),X.w6_1x(:,2),'k.');
@@ -24,7 +24,7 @@ for t = 1:tmax
         prototype_values(idx,:) = prototype_values(idx,:) + eta * (point - prototype_values(idx,:));
     end
     p3 = scatter(prototype_values(idx,1),prototype_values(idx,2),'g.');
-    drawnow;
+    %drawnow;
 end
 p4 = scatter(prototype_values(:,1),prototype_values(:,2),'m*');
 title('Winner-takes-all implementation on w6_1x set for 4 prototypes', 'Interpreter', 'None');
@@ -33,5 +33,8 @@ legend(h,{'Data points', 'Initial prototypes', 'Prototypes in each iteration', '
 hold off;
 figure(2); hold on;
 plot(h_vq);
+ylabel('Quantization Error');
+xlabel('Epoch');
+title('Quantization Error for K=2 and eta=0.005 for 200 epochs');
 hold off;
 end
